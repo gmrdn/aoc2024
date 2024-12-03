7f19bf666a18d0126ad84d91737760d082efa383
gmrdn <guillaume.rondon@gmail.com>
2024-12-03 09:13:53 +0100

    day3: split string, find regexes

--- d3/d.go
+++ d3/d.go
@@ -3,6 +3,9 @@ package d
 import (
 	"bufio"
 	"io"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 type D struct {
@@ -17,8 +20,43 @@ func (d *D) Input(input io.Reader) {
 	d.inputStream = bufio.NewReader(input)
 }
 
-func doSomething() int {
-	return 0
+func doSomething1(fileScanner *bufio.Scanner) int {
+	result := 0
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+
+		mulMatches := regexp.MustCompile(`mul\((\d+),(\d+)\)`).FindAllStringSubmatch(line, -1)
+		for _, match := range mulMatches {
+			num1, _ := strconv.Atoi(match[1])
+			num2, _ := strconv.Atoi(match[2])
+			result += num1 * num2
+		}
+	}
+	return result
+}
+
+func doSomething2(fileScanner *bufio.Scanner) int {
+	result := 0
+	bigLine := ""
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		bigLine += line
+	}
+
+	instructionsStartingWithDo := strings.Split(bigLine, "do()")
+	for _, instruction := range instructionsStartingWithDo {
+		splitOnDont := strings.Split(instruction, "don't()")
+		doPart := splitOnDont[0]
+
+		mulMatches := regexp.MustCompile(`mul\((\d+),(\d+)\)`).FindAllStringSubmatch(doPart, -1)
+		for _, match := range mulMatches {
+			num1, _ := strconv.Atoi(match[1])
+			num2, _ := strconv.Atoi(match[2])
+			result += num1 * num2
+		}
+
+	}
+	return result
 }
 
 func (d *D) Run1() int {
@@ -26,7 +64,7 @@ func (d *D) Run1() int {
 
 	fileScanner.Split(bufio.ScanLines)
 
-	return doSomething()
+	return doSomething1(fileScanner)
 }
 
 func (d *D) Run2() int {
@@ -34,7 +72,7 @@ func (d *D) Run2() int {
 
 	fileScanner.Split(bufio.ScanLines)
 
-	return doSomething()
+	return doSomething2(fileScanner)
 }
 
 func (d *D) RunStr1() string {
--- d3/d_test.go
+++ d3/d_test.go
@@ -6,35 +6,24 @@ import (
 )
 
 func TestD(t *testing.T) {
-	input := `7 6 4 2 1
-1 2 7 8 9
-9 7 6 2 1
-1 3 2 4 5
-8 6 4 4 1
-1 3 6 7 9`
+	input := `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`
 
 	d := NewD()
 	d.Input(bytes.NewBufferString(input))
 	got := d.Run1()
-	want := 1
+	want := 161
 	if got != want {
 		t.Errorf("got %v want %v", got, want)
 	}
 }
 
 func TestD2(t *testing.T) {
-	input := `7 6 4 2 1
-1 2 7 8 9
-9 7 6 2 1
-1 3 2 4 5
-8 6 4 4 1
-1 3 6 7 9
-1 2 3 4 5`
+	input := `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`
 
 	d := NewD()
 	d.Input(bytes.NewBufferString(input))
 	got := d.Run2()
-	want := 1
+	want := 48
 	if got != want {
 		t.Errorf("got %v want %v", got, want)
 	}
