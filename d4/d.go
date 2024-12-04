fe75b8a9b7a04ab4af55a7912246363f956edb2a
gmrdn <guillaume.rondon@gmail.com>
2024-12-04 09:22:57 +0100

    day4: read matrix in all directions

--- d4/d.go
+++ d4/d.go
@@ -3,6 +3,7 @@ package d
 import (
 	"bufio"
 	"io"
+	"strings"
 )
 
 type D struct {
@@ -32,7 +33,95 @@ func (d *D) Run1() int {
 
 	fileScanner.Split(bufio.ScanLines)
 
-	return doSomething1(fileScanner)
+	matrix := [][]string{}
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		matrix = append(matrix, strings.Split(line, ""))
+	}
+
+	nbXMAS := 0
+	for i, row := range matrix {
+		for j, col := range row {
+			if col == "X" {
+				// up
+				wordUp := []string{}
+				if i-3 >= 0 {
+					wordUp = append(wordUp, matrix[i-1][j])
+					wordUp = append(wordUp, matrix[i-2][j])
+					wordUp = append(wordUp, matrix[i-3][j])
+					if strings.Join(wordUp, "") == "MAS" {
+						nbXMAS++
+					}
+				}
+				wordUpRight := []string{}
+				if i-3 >= 0 && j+3 < len(row) {
+					wordUpRight = append(wordUpRight, matrix[i-1][j+1])
+					wordUpRight = append(wordUpRight, matrix[i-2][j+2])
+					wordUpRight = append(wordUpRight, matrix[i-3][j+3])
+					if strings.Join(wordUpRight, "") == "MAS" {
+						nbXMAS++
+					}
+				}
+				wordRight := []string{}
+				if j+3 < len(row) {
+					wordRight = append(wordRight, matrix[i][j+1])
+					wordRight = append(wordRight, matrix[i][j+2])
+					wordRight = append(wordRight, matrix[i][j+3])
+					if strings.Join(wordRight, "") == "MAS" {
+						nbXMAS++
+					}
+				}
+				wordDownRight := []string{}
+				if i+3 < len(matrix) && j+3 < len(row) {
+					wordDownRight = append(wordDownRight, matrix[i+1][j+1])
+					wordDownRight = append(wordDownRight, matrix[i+2][j+2])
+					wordDownRight = append(wordDownRight, matrix[i+3][j+3])
+					if strings.Join(wordDownRight, "") == "MAS" {
+						nbXMAS++
+					}
+				}
+				wordDown := []string{}
+				if i+3 < len(matrix) {
+					wordDown = append(wordDown, matrix[i+1][j])
+					wordDown = append(wordDown, matrix[i+2][j])
+					wordDown = append(wordDown, matrix[i+3][j])
+					if strings.Join(wordDown, "") == "MAS" {
+						nbXMAS++
+					}
+				}
+				wordDownLeft := []string{}
+				if i+3 < len(matrix) && j-3 >= 0 {
+					wordDownLeft = append(wordDownLeft, matrix[i+1][j-1])
+					wordDownLeft = append(wordDownLeft, matrix[i+2][j-2])
+					wordDownLeft = append(wordDownLeft, matrix[i+3][j-3])
+					if strings.Join(wordDownLeft, "") == "MAS" {
+						nbXMAS++
+					}
+				}
+				wordLeft := []string{}
+				if j-3 >= 0 {
+					wordLeft = append(wordLeft, matrix[i][j-1])
+					wordLeft = append(wordLeft, matrix[i][j-2])
+					wordLeft = append(wordLeft, matrix[i][j-3])
+					if strings.Join(wordLeft, "") == "MAS" {
+						nbXMAS++
+					}
+				}
+				wordUpLeft := []string{}
+				if i-3 >= 0 && j-3 >= 0 {
+					wordUpLeft = append(wordUpLeft, matrix[i-1][j-1])
+					wordUpLeft = append(wordUpLeft, matrix[i-2][j-2])
+					wordUpLeft = append(wordUpLeft, matrix[i-3][j-3])
+					if strings.Join(wordUpLeft, "") == "MAS" {
+						nbXMAS++
+					}
+				}
+
+			}
+		}
+	}
+
+	return nbXMAS
 }
 
 func (d *D) Run2() int {
@@ -40,7 +129,37 @@ func (d *D) Run2() int {
 
 	fileScanner.Split(bufio.ScanLines)
 
-	return doSomething2(fileScanner)
+	matrix := [][]string{}
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		matrix = append(matrix, strings.Split(line, ""))
+	}
+
+	nbXMAS := 0
+	for i, row := range matrix {
+		for j, col := range row {
+			if col == "A" {
+				wordDiagonalRight := []string{}
+				wordDiagonalLeft := []string{}
+				if i-1 >= 0 && i+1 < len(matrix) && j-1 >= 0 && j+1 < len(row) {
+					wordDiagonalRight = append(wordDiagonalRight, matrix[i-1][j-1])
+					wordDiagonalRight = append(wordDiagonalRight, "A")
+					wordDiagonalRight = append(wordDiagonalRight, matrix[i+1][j+1])
+					wordDiagonalLeft = append(wordDiagonalLeft, matrix[i-1][j+1])
+					wordDiagonalLeft = append(wordDiagonalLeft, "A")
+					wordDiagonalLeft = append(wordDiagonalLeft, matrix[i+1][j-1])
+
+					if strings.Join(wordDiagonalRight, "") == "MAS" || strings.Join(wordDiagonalRight, "") == "SAM" {
+						if strings.Join(wordDiagonalLeft, "") == "MAS" || strings.Join(wordDiagonalLeft, "") == "SAM" {
+							nbXMAS++
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return nbXMAS
 }
 
 func (d *D) RunStr1() string {
--- d4/d_test.go
+++ d4/d_test.go
@@ -5,25 +5,43 @@ import (
 	"testing"
 )
 
-// func TestD(t *testing.T) {
-// 	input := `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`
-//
-// 	d := NewD()
-// 	d.Input(bytes.NewBufferString(input))
-// 	got := d.Run1()
-// 	want := 161
-// 	if got != want {
-// 		t.Errorf("got %v want %v", got, want)
-// 	}
-// }
+func TestD(t *testing.T) {
+	input := `MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX`
+
+	d := NewD()
+	d.Input(bytes.NewBufferString(input))
+	got := d.Run1()
+	want := 18
+	if got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
 
 func TestD2(t *testing.T) {
-	input := `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`
+	input := `MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX`
 
 	d := NewD()
 	d.Input(bytes.NewBufferString(input))
 	got := d.Run2()
-	want := 48
+	want := 9
 	if got != want {
 		t.Errorf("got %v want %v", got, want)
 	}
