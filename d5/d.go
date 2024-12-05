741862af815ed941e6ddbd214039101fc2fd2c90
gmrdn <guillaume.rondon@gmail.com>
2024-12-05 08:57:13 +0100

    day5: custom sort by map containing element

--- d5/d.go
+++ d5/d.go
@@ -2,8 +2,9 @@ package d
 
 import (
 	"bufio"
-	"fmt"
 	"io"
+	"slices"
+	"strconv"
 	"strings"
 )
 
@@ -34,95 +35,54 @@ func (d *D) Run1() int {
 
 	fileScanner.Split(bufio.ScanLines)
 
-	matrix := [][]string{}
+	fullText := ""
 	for fileScanner.Scan() {
 		line := fileScanner.Text()
-		matrix = append(matrix, strings.Split(line, ""))
+		fullText += line + "\n"
 	}
 
-	nbXMAS := 0
-	for i, row := range matrix {
-		for j, col := range row {
-			if col == "X" {
-				// up
-				wordUp := []string{}
-				if i-3 >= 0 {
-					wordUp = append(wordUp, matrix[i-1][j])
-					wordUp = append(wordUp, matrix[i-2][j])
-					wordUp = append(wordUp, matrix[i-3][j])
-					if strings.Join(wordUp, "") == "MAS" {
-						nbXMAS++
-					}
-				}
-				wordUpRight := []string{}
-				if i-3 >= 0 && j+3 < len(row) {
-					wordUpRight = append(wordUpRight, matrix[i-1][j+1])
-					wordUpRight = append(wordUpRight, matrix[i-2][j+2])
-					wordUpRight = append(wordUpRight, matrix[i-3][j+3])
-					if strings.Join(wordUpRight, "") == "MAS" {
-						nbXMAS++
-					}
-				}
-				wordRight := []string{}
-				if j+3 < len(row) {
-					wordRight = append(wordRight, matrix[i][j+1])
-					wordRight = append(wordRight, matrix[i][j+2])
-					wordRight = append(wordRight, matrix[i][j+3])
-					if strings.Join(wordRight, "") == "MAS" {
-						nbXMAS++
-					}
-				}
-				wordDownRight := []string{}
-				if i+3 < len(matrix) && j+3 < len(row) {
-					wordDownRight = append(wordDownRight, matrix[i+1][j+1])
-					wordDownRight = append(wordDownRight, matrix[i+2][j+2])
-					wordDownRight = append(wordDownRight, matrix[i+3][j+3])
-					if strings.Join(wordDownRight, "") == "MAS" {
-						nbXMAS++
-					}
-				}
-				wordDown := []string{}
-				if i+3 < len(matrix) {
-					wordDown = append(wordDown, matrix[i+1][j])
-					wordDown = append(wordDown, matrix[i+2][j])
-					wordDown = append(wordDown, matrix[i+3][j])
-					if strings.Join(wordDown, "") == "MAS" {
-						nbXMAS++
-					}
-				}
-				wordDownLeft := []string{}
-				if i+3 < len(matrix) && j-3 >= 0 {
-					wordDownLeft = append(wordDownLeft, matrix[i+1][j-1])
-					wordDownLeft = append(wordDownLeft, matrix[i+2][j-2])
-					wordDownLeft = append(wordDownLeft, matrix[i+3][j-3])
-					if strings.Join(wordDownLeft, "") == "MAS" {
-						nbXMAS++
-					}
-				}
-				wordLeft := []string{}
-				if j-3 >= 0 {
-					wordLeft = append(wordLeft, matrix[i][j-1])
-					wordLeft = append(wordLeft, matrix[i][j-2])
-					wordLeft = append(wordLeft, matrix[i][j-3])
-					if strings.Join(wordLeft, "") == "MAS" {
-						nbXMAS++
-					}
-				}
-				wordUpLeft := []string{}
-				if i-3 >= 0 && j-3 >= 0 {
-					wordUpLeft = append(wordUpLeft, matrix[i-1][j-1])
-					wordUpLeft = append(wordUpLeft, matrix[i-2][j-2])
-					wordUpLeft = append(wordUpLeft, matrix[i-3][j-3])
-					if strings.Join(wordUpLeft, "") == "MAS" {
-						nbXMAS++
-					}
-				}
+	sections := strings.Split(fullText, "\n\n")
 
-			}
+	firstSection := sections[0]
+	secondSection := sections[1]
+
+	shouldBeBefore := make(map[int][]int)
+	for _, line := range strings.Split(firstSection, "\n") {
+		parts := strings.Split(line, "|")
+		first, _ := strconv.Atoi(parts[0])
+		second, _ := strconv.Atoi(parts[1])
+		shouldBeBefore[first] = append(shouldBeBefore[first], second)
+	}
+
+	result := 0
+
+	for _, line := range strings.Split(secondSection, "\n") {
+		values := strings.Split(line, ",")
+		numbers := []int{}
+		for _, value := range values {
+			number, _ := strconv.Atoi(value)
+			numbers = append(numbers, number)
 		}
-	}
 
-	return nbXMAS
+		numbersBeforeSorting := slices.Clone(numbers)
+		sortedNumbers := sortWithRules(numbers, shouldBeBefore)
+
+		if slices.Equal(numbersBeforeSorting, sortedNumbers) {
+			result += numbersBeforeSorting[len(numbersBeforeSorting)/2]
+		}
+
+	}
+	return result
+}
+
+func sortWithRules(numbers []int, shouldBeBefore map[int][]int) []int {
+	slices.SortFunc(numbers, func(a, b int) int {
+		if slices.Contains(shouldBeBefore[a], b) {
+			return -1
+		}
+		return 1
+	})
+	return numbers
 }
 
 func (d *D) Run2() int {
@@ -130,40 +90,44 @@ func (d *D) Run2() int {
 
 	fileScanner.Split(bufio.ScanLines)
 
-	matrix := [][]string{}
+	fullText := ""
 	for fileScanner.Scan() {
 		line := fileScanner.Text()
-		matrix = append(matrix, strings.Split(line, ""))
+		fullText += line + "\n"
 	}
 
-	nbXMAS := 0
-	for i, row := range matrix {
-		for j, col := range row {
-			if col == "A" {
-				wordDiagonalRight := []string{}
-				wordDiagonalLeft := []string{}
-				if i-1 >= 0 && i+1 < len(matrix) && j-1 >= 0 && j+1 < len(row) {
-					fmt.Println("trying A at", i, j)
-					wordDiagonalRight = append(wordDiagonalRight, matrix[i-1][j-1])
-					wordDiagonalRight = append(wordDiagonalRight, "A")
-					wordDiagonalRight = append(wordDiagonalRight, matrix[i+1][j+1])
-					fmt.Println("wordDiagonalRight", wordDiagonalRight)
-					wordDiagonalLeft = append(wordDiagonalLeft, matrix[i-1][j+1])
-					wordDiagonalLeft = append(wordDiagonalLeft, "A")
-					wordDiagonalLeft = append(wordDiagonalLeft, matrix[i+1][j-1])
-					fmt.Println("wordDiagonalLeft", wordDiagonalLeft)
+	sections := strings.Split(fullText, "\n\n")
 
-					if strings.Join(wordDiagonalRight, "") == "MAS" || strings.Join(wordDiagonalRight, "") == "SAM" {
-						if strings.Join(wordDiagonalLeft, "") == "MAS" || strings.Join(wordDiagonalLeft, "") == "SAM" {
-							nbXMAS++
-						}
-					}
-				}
-			}
+	firstSection := sections[0]
+	secondSection := sections[1]
+
+	shouldBeBefore := make(map[int][]int)
+	for _, line := range strings.Split(firstSection, "\n") {
+		parts := strings.Split(line, "|")
+		first, _ := strconv.Atoi(parts[0])
+		second, _ := strconv.Atoi(parts[1])
+		shouldBeBefore[first] = append(shouldBeBefore[first], second)
+	}
+
+	result := 0
+
+	for _, line := range strings.Split(secondSection, "\n") {
+		values := strings.Split(line, ",")
+		numbers := []int{}
+		for _, value := range values {
+			number, _ := strconv.Atoi(value)
+			numbers = append(numbers, number)
 		}
-	}
 
-	return nbXMAS
+		numbersBeforeSorting := slices.Clone(numbers)
+		sortedNumbers := sortWithRules(numbers, shouldBeBefore)
+
+		if !slices.Equal(numbersBeforeSorting, sortedNumbers) {
+			result += sortedNumbers[len(sortedNumbers)/2]
+		}
+
+	}
+	return result
 }
 
 func (d *D) RunStr1() string {
--- d5/d_test.go
+++ d5/d_test.go
@@ -6,43 +6,79 @@ import (
 )
 
 func TestD(t *testing.T) {
-	input := `MMMSXXMASM
-MSAMXMSMSA
-AMXSXMAAMM
-MSAMASMSMX
-XMASAMXAMM
-XXAMMXXAMA
-SMSMSASXSS
-SAXAMASAAA
-MAMMMXMMMM
-MXMXAXMASX`
+	input := `47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47`
 
 	d := NewD()
 	d.Input(bytes.NewBufferString(input))
 	got := d.Run1()
-	want := 18
+	want := 143
 	if got != want {
 		t.Errorf("got %v want %v", got, want)
 	}
 }
 
-// func TestD2(t *testing.T) {
-// 	input := `MMMSXXMASM
-// MSAMXMSMSA
-// AMXSXMAAMM
-// MSAMASMSMX
-// XMASAMXAMM
-// XXAMMXXAMA
-// SMSMSASXSS
-// SAXAMASAAA
-// MAMMMXMMMM
-// MXMXAXMASX`
-//
-// 	d := NewD()
-// 	d.Input(bytes.NewBufferString(input))
-// 	got := d.Run2()
-// 	want := 9
-// 	if got != want {
-// 		t.Errorf("got %v want %v", got, want)
-// 	}
-// }
+func TestD2(t *testing.T) {
+	input := `47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47`
+
+	d := NewD()
+	d.Input(bytes.NewBufferString(input))
+	got := d.Run2()
+	want := 123
+	if got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
