fa01e3109e24f97075f65d725a85970c4b461319
gmrdn <guillaume.rondon@gmail.com>
2024-12-06 14:32:46 +0100

    day6: solved part 1 but part 2 is buggy

--- d6/d.go
+++ d6/d.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"strings"
 )
 
 type D struct {
@@ -18,16 +19,134 @@ func (d *D) Input(input io.Reader) {
 	d.inputStream = bufio.NewReader(input)
 }
 
+type point struct {
+	x, y int
+}
+type state struct {
+	grid                     [][]string
+	pos                      point
+	direction                int // 8 = up, 6 = right, 2 = down, 4 = left
+	visited                  []point
+	obstaclesMetByDirections map[int][]point
+}
+
+var nextDirection = map[int]int{
+	8: 6,
+	6: 2,
+	2: 4,
+	4: 8,
+}
+
+func (s *state) getNextPosition() (point, bool) {
+	nextPosition := s.pos
+	switch s.direction {
+	case 8:
+		nextPosition.x -= 1
+	case 6:
+		nextPosition.y += 1
+	case 2:
+		nextPosition.x += 1
+	case 4:
+		nextPosition.y -= 1
+	}
+
+	obstacle := false
+	if nextPosition.x < 0 || nextPosition.x >= len(s.grid) || nextPosition.y < 0 || nextPosition.y >= len(s.grid[0]) {
+		// fmt.Println("out of bounds")
+		// fmt.Println(nextPosition)
+		return nextPosition, false
+	}
+
+	if s.grid[nextPosition.x][nextPosition.y] == "#" {
+		// fmt.Println("obstacle")
+		obstacle = true
+		obstaclePosition := point{nextPosition.x, nextPosition.y}
+
+		for _, alreadyMet := range s.obstaclesMetByDirections[s.direction] {
+			if alreadyMet == obstaclePosition {
+				fmt.Println("infinite loop caused by obstacle", obstaclePosition)
+				return nextPosition, true
+			}
+		}
+
+		s.obstaclesMetByDirections[s.direction] = append(s.obstaclesMetByDirections[s.direction], obstaclePosition)
+
+		newDirection := nextDirection[s.direction]
+		s.direction = newDirection
+	}
+
+	if obstacle {
+		nextPosition = s.pos
+		switch s.direction {
+		case 8:
+			nextPosition.x -= 1
+		case 6:
+			nextPosition.y += 1
+		case 2:
+			nextPosition.x += 1
+		case 4:
+			nextPosition.y -= 1
+		}
+	}
+
+	return nextPosition, false
+}
+
 func (d *D) Run1() int {
 	fileScanner := bufio.NewScanner(d.inputStream)
 	fileScanner.Split(bufio.ScanLines)
 
+	grid := [][]string{}
 	for fileScanner.Scan() {
 		line := fileScanner.Text()
-		fmt.Println(line)
+		grid = append(grid, strings.Split(line, ""))
 	}
 
-	result := 0
+	startingPosition := point{0, 0}
+
+	for i, row := range grid {
+		// fmt.Println(row)
+		for j, cell := range row {
+			if cell == "^" {
+				startingPosition = point{i, j}
+			}
+		}
+	}
+
+	state := state{
+		pos:                      startingPosition,
+		direction:                8,
+		visited:                  []point{},
+		grid:                     grid,
+		obstaclesMetByDirections: map[int][]point{},
+	}
+
+	for {
+		nextPosition, _ := state.getNextPosition()
+
+		state.visited = append(state.visited, state.pos)
+		state.pos = nextPosition
+		// fmt.Println(state.visited)
+
+		if nextPosition.x < 0 || nextPosition.x >= len(grid) || nextPosition.y < 0 || nextPosition.y >= len(grid[0]) {
+			break
+		}
+	}
+
+	for _, visit := range state.visited {
+		grid[visit.x][visit.y] = "X"
+	}
+
+	// for _, row := range grid {
+	// fmt.Println(row)
+	// }
+
+	uniqueVisited := map[point]bool{}
+	for _, visit := range state.visited {
+		uniqueVisited[visit] = true
+	}
+
+	result := len(uniqueVisited)
 
 	return result
 }
@@ -36,12 +155,73 @@ func (d *D) Run2() int {
 	fileScanner := bufio.NewScanner(d.inputStream)
 	fileScanner.Split(bufio.ScanLines)
 
+	grid := [][]string{}
 	for fileScanner.Scan() {
 		line := fileScanner.Text()
-		fmt.Println(line)
+		grid = append(grid, strings.Split(line, ""))
 	}
 
-	result := 0
+	startingPosition := point{0, 0}
+
+	for i, row := range grid {
+		// fmt.Println(row)
+		for j, cell := range row {
+			if cell == "^" {
+				startingPosition = point{i, j}
+			}
+		}
+	}
+
+	// run simulations
+
+	nbInfiniteLoops := 0
+	for i, row := range grid {
+		for j, cell := range row {
+			if cell == "." {
+
+				simulationGrid := make([][]string, len(grid))
+				for i := range grid {
+					simulationGrid[i] = make([]string, len(grid[i]))
+					copy(simulationGrid[i], grid[i])
+				}
+
+				obstacleCell := "#"
+				simulationGrid[i][j] = obstacleCell
+
+				state := state{
+					pos:       startingPosition,
+					direction: 8,
+					visited:   []point{},
+					grid:      simulationGrid,
+					obstaclesMetByDirections: map[int][]point{
+						8: {},
+						6: {},
+						2: {},
+						4: {},
+					},
+				}
+
+				for {
+					nextPosition, infiniteLoop := state.getNextPosition()
+
+					if infiniteLoop {
+						nbInfiniteLoops++
+						break
+					}
+
+					state.visited = append(state.visited, state.pos)
+					state.pos = nextPosition
+					// fmt.Println(state.visited)
+
+					if nextPosition.x < 0 || nextPosition.x >= len(grid) || nextPosition.y < 0 || nextPosition.y >= len(grid[0]) {
+						break
+					}
+				}
+			}
+		}
+	}
+
+	result := nbInfiniteLoops
 
 	return result
 }
--- d6/d_test.go
+++ d6/d_test.go
@@ -5,28 +5,44 @@ import (
 	"testing"
 )
 
-func TestD(t *testing.T) {
-	input := `47|53
-97,13,75,29,47`
-
-	d := NewD()
-	d.Input(bytes.NewBufferString(input))
-	got := d.Run1()
-	want := 1
-	if got != want {
-		t.Errorf("got %v want %v", got, want)
-	}
-}
-
-// func TestD2(t *testing.T) {
-// 	input := `47|53
-// 97,13,75,29,47`
+// func TestD(t *testing.T) {
+// 	input := `....#.....
+// .........#
+// ..........
+// ..#.......
+// .......#..
+// ..........
+// .#..^.....
+// ........#.
+// #.........
+// ......#...`
 //
 // 	d := NewD()
 // 	d.Input(bytes.NewBufferString(input))
-// 	got := d.Run2()
-// 	want := 1
+// 	got := d.Run1()
+// 	want := 41
 // 	if got != want {
 // 		t.Errorf("got %v want %v", got, want)
 // 	}
 // }
+
+func TestD2(t *testing.T) {
+	input := `....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...`
+
+	d := NewD()
+	d.Input(bytes.NewBufferString(input))
+	got := d.Run2()
+	want := 6
+	if got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
